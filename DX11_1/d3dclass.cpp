a685ad7961fd02e3747cc6ba3c286f5146b9d4da
hym <601751610@qq.com>
2023-12-27 00:44:27 +0800

    no message

--- DX11_1/d3dclass.cpp
+++ DX11_1/d3dclass.cpp
@@ -45,7 +45,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool vsync, HWND hw
 	DXGI_SWAP_CHAIN_DESC swapChainDesc;//描述交换链
 	D3D_FEATURE_LEVEL featureLevel;//Direct3D 设备面向的功能集枚举
 	ID3D11Texture2D* backBufferPtr;//纹理数据
-	D3D11_TEXTURE2D_DESC depthBufferDesc;//纹理描述
+	D3D11_TEXTURE2D_DESC depthBufferDesc;//深度纹理描述
 	D3D11_DEPTH_STENCIL_DESC depthStencilDesc;//深度模板状态
 	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc;//指定可从深度模具视图访问的纹理子资源
 	D3D11_RASTERIZER_DESC rasterDesc;//光栅器状态
@@ -217,7 +217,88 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool vsync, HWND hw
 	Direct3D设备和Direct3D设备上下文非常重要，它们是所有Direct3D功能的接口。
 	从这一点开始，我们将对几乎所有内容使用设备和设备上下文。*/
 
-	return false;
+	//创建表示显示适配器的设备以及用于呈现的交换链。
+	result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, &featureLevel, 1,
+		D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, NULL, &m_deviceContext);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	/*有时，如果主视频卡与DirectX 11不兼容，则创建设备的调用将失败。
+	某些机器可能将主卡作为DirectX 10视频卡，将次卡作为DirectX 11视频卡。
+	此外，一些混合显卡也是这样工作的，主要是低功耗的英特尔卡，次要是高功耗的英伟达卡。
+	要解决此问题，需要不使用默认设备，而是枚举机器中的所有视频卡，并让用户选择要使用的视频卡，
+	然后在创建设备时指定该卡。*/
+	/*现在我们有了交换链，我们需要获得一个指向后缓冲区的指针，然后将其连接到交换链。
+	我们将使用CreateRenderTargetView函数将后台缓冲区附加到交换链。*/
+
+	//获取指向后缓冲区的指针
+	result = m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBufferPtr);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	//使用后缓冲区指针创建渲染目标视图。
+	result = m_device->CreateRenderTargetView(backBufferPtr, NULL, &m_renderTargetView);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	//释放指向后缓冲区的指针，因为我们不再需要它。
+	backBufferPtr->Release();
+	backBufferPtr = 0;
+
+	/*还需要设置一个深度缓冲区描述。将使用它来创建深度缓冲区，以便在3D空间中正确渲染多边形。
+	同时，将在深度缓冲区中附加一个模版缓冲区。模具缓冲区可用于实现运动模糊、体积阴影等效果。*/
+
+	//初始化深度缓冲区描述。
+	ZeroMemory(&depthBufferDesc, sizeof(depthBufferDesc));
+
+	//设置深度缓冲区描述
+	depthBufferDesc.Width = screenWidth;
+	depthBufferDesc.Height = screenHeight;
+	depthBufferDesc.MipLevels = 1;
+	depthBufferDesc.ArraySize = 1;
+	depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;//32位z缓冲区格式，支持24位深度和8位模具
+	depthBufferDesc.SampleDesc.Count = 1;//为纹理指定多重采样参数的结构的每个像素的多样本数为1
+	depthBufferDesc.SampleDesc.Quality = 0;  //图像质量级别。 质量越高，性能越低.有效范围介于 0 到 1 之间
+	depthBufferDesc.Usage = D3D11_USAGE_DEFAULT;//标识如何读取和写入纹理的值.【D3D11_USAGE_DEFAULT】表示需要 GPU 的读取和写入访问权限的资源
+	depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;//标识如何将资源绑定到管道,【D3D11_BIND_DEPTH_STENCIL】将纹理绑定为输出合并阶段的深度模具目标。
+	depthBufferDesc.CPUAccessFlags = 0;//以指定允许的 CPU 访问类型的标志,0表示不需要CPU访问
+	
+	/* 对于纹理立方体贴图，设置 D3D11_RESOURCE_MISC_TEXTURECUBE 标志。 
+	多维数据集映射数组 (即 ArraySize> 6) 需要 功能级别D3D_FEATURE_LEVEL_10_1 或更高。*/
+	depthBufferDesc.MiscFlags = 0;//标志
+
+	/*使用该描述创建深度/模具缓冲区。
+	使用CreateTexture2D函数来制作缓冲区，因此缓冲区只是一个2D纹理。
+	这样做的原因是，一旦对多边形进行排序并光栅化，它们最终就会成为2D缓冲区中的彩色像素。然后，这个2D缓冲区被绘制到屏幕上*/
+	//使用填充的描述为深度缓冲区创建纹理
+	result = m_device->CreateTexture2D(&depthBufferDesc, NULL, &m_depthStencilBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	/*现在需要设置深度模具描述。用于控制Direct3D将对每个像素进行何种类型的深度测试。*/
+	ZeroMemory(&depthStencilDesc, sizeof(depthStencilDesc));
+	depthStencilDesc.DepthEnable = true;//启用深度测试
+	depthStencilDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;//用于写入深度数据的深度模具缓冲区部分,【D3D11_DEPTH_WRITE_MASK_ALL】表示启用对深度模具缓冲区的写入
+	depthStencilDesc.DepthFunc = D3D11_COMPARISON_LESS;//用于将深度数据与现有深度数据进行比较的函数，【D3D11_COMPARISON_LESS】指示更小值靠前
+	depthStencilDesc.StencilEnable = true;//启用摸具测试
+	depthStencilDesc.StencilReadMask = 0xFF;
+	depthStencilDesc.StencilWriteMask = 0xFF;
+	//对正面像素进行的模具操作
+	depthStencilDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;//模具测试失败时要执行的模具操作,【..】保留现有模具数据。
+	depthStencilDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;//模具测试通过且深度测试失败时要执行的模具操作【..】将模具值递增 1，如有必要，将结果换行
+	depthStencilDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;//模具测试和深度测试都通过时要执行的模具操作,【..】
+	depthStencilDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;//将模具数据与现有模具数据进行比较，【..】始终传递比较
+
+
+	return true;
 }
 
 void D3DClass::Shutdown()
